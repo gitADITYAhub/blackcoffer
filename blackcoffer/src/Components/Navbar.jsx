f9dbf6dec393279434981d7168a62a1fd37e0a6a
gitADITYAhub <adityasinghhvm10@gmail.com>
2024-03-04 18:09:22 +0530

    first commit

---
+++ blackcoffer/src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+export const Navbar = () => {
+  return (
+    <div
+      style={{
+        width: "100%",
+        textAlign: "center",
+      }}
+    >
+      <img
+        src="https://blackcoffer.com/wp-content/uploads/2022/02/Blackcoffer-logo-new.png"
+        alt="blackcoffer"
+        width={"10%"}
+      />
+    </div>
+  );
+};
